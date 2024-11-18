5bd13288c2b140e505f46a6f438f41bfe2c4d077
DELL <sharandey@gmail.com>
2024-11-18 17:25:27 +1030

    multipication

---
+++ feature-z.py
@@ -0,0 +1,6 @@
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+result = num1 * num2
+print(f"The value after Multipication of {num1} and {num2} is: {result}")
+
+#fff
