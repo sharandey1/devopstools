a32bd36639d2aba8155cda2d4bb80fad027df2e1
DELL <sharandey@gmail.com>
2024-11-18 19:13:55 +1030

    test-task

---
+++ unit-test.py
@@ -0,0 +1,39 @@
+import unittest 
+def add_numbers(a,b):
+    return a+b
+class TestAddNumbers(unittest.TestCase):
+    def test_add_numbers(self):
+        result = add_numbers(2,3)
+        self.assertEqual(result,5)
+
+def deduct_numbers(a,b):
+    return a-b
+class TestDeductNumbers(unittest.TestCase):
+    def test_deduct_numbers(self):
+        result = deduct_numbers(2,3)
+        self.assertEqual(result,-1)
+
+def multiply_numbers(a,b):
+    return a*b
+class TestMultiplyNumbers(unittest.TestCase):
+    def test_multiply_numbers(self):
+        result = multiply_numbers(2,3)
+        self.assertEqual(result,6)
+
+def divide_numbers(a,b):
+    return a/b
+class TestDivideNumbers(unittest.TestCase):
+    def test_divide_numbers(self):
+        result = divide_numbers(2,3)
+        self.assertEqual(result,5)
+
+def add_numbers2(a,b):
+    return a+b
+class TestAddNumbers(unittest.TestCase):
+    def test_add_numbers2(self):
+        result = add_numbers2(2,3)
+        self.assertEqual(result,6)
+
+if __name__=='__main__':
+    unittest.main()
+
