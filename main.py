24e1ae8c7b9b70a01d68989cde11b9f642da9430
DELL <sharandey@gmail.com>
2024-11-18 16:49:21 +1030

    first commit

---
+++ main.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Nov 18 16:36:40 2024
+
+@author: DELL
+"""
+
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+result = num1 + num2
+print(f"The sum of {num1} and {num2} is: {result}")
+
+       
